7d4cfbf545ca7759384437ab743ca72388024609
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-06-28 15:49:42 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -40,9 +40,9 @@ Profile.propTypes = {
     location:PropTypes.string.isRequired,
     stats:PropTypes.arrayOf(
       PropTypes.shape({
-         followers:PropTypes.number,
-         views:PropTypes.number,
-         likes:PropTypes.number,
+         followers:PropTypes.number.isRequired,
+         views:PropTypes.number.isRequired,
+         likes:PropTypes.number.isRequired,
       })
      )
 };
